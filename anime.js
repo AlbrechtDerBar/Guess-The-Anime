b649f82a20d107402842a1b98ee52b9f71d1f1ba
AlbrechtDerBar <rokumura20@gmail.com>
2021-03-18 13:09:37 -0700

    bruh

--- anime.js
+++ anime.js
@@ -3,23 +3,22 @@ var rnd = 0;
 var rndPrev = 0;
 var startTime = 0;
 
-let videos = ["SohCAjC-Npk", "0H_RCGEcjhs", "tF4faMbs5oQ"];
-let titles = ["Dr. Stone", "inward", "Yeuh"];
-let songTitles = ["\"Sangenshoku (三原色)\" by PELICAN FANCLUB"]
+let videos = ["vS_a8Edde8k", "-WpnPSChVRQ", "Jrg9KxGNeJY"];
+let titles = ["Anime 1", "DMC5", "Devil May Cry 5"];
+let songTitles = ["night of nights", "Devil Trigger", "Bury the Light"]
 
 function init() {
     var prevbutton = document.getElementById("prev");
+    var settingsbutton = document.getElementById("settings-button");
     var playbutton = document.getElementById("play-pause");
     var replaybutton = document.getElementById("replay");
     var nextbutton = document.getElementById("next");
-    // var randomStart = document.getElementById("random");
-    // var navbutton = document.getElementById("nav");
 
     prevbutton.addEventListener("click", prev);
+    settingsbutton.addEventListener("click", settings);
     playbutton.addEventListener("click", playPause);
     replaybutton.addEventListener("click", replay);
     nextbutton.addEventListener("click", next);
-    // navbutton.addEventListener("click", settings);
 }
 
 var player;
@@ -43,11 +42,11 @@ function onYouTubeIframeAPIReady() {
 }
 
 
-// random function for video and start time
+// random function for video
 function rndVideo() {
-    return Math.floor(Math.random() * videos.length);
+    return Math.floor(Math.random() * (videos.length));
 }
-
+//random function for start time
 function rndTime() {
     return (15 + Math.floor(Math.random() * 15));
 }
@@ -56,13 +55,13 @@ function rndTime() {
 // functions for the music controls
 function next() {
     rnd = rndVideo();
-    startTime = rndStart();
+    // startTime = rndStart();
+    rndStart();
 
-    while (rnd == rndPrev) {
-        rnd = rndVideo();
-    }
-    // rndPrev = rnd;
     document.getElementById("anime-title").innerHTML = titles[rnd]
+    if (document.getElementById("includeSongName").checked) {
+        document.getElementById("song-title").innerHTML = songTitles[rnd]
+    }
     if (player.getPlayerState() == 1) {
         player.loadVideoById(videos[rnd], startTime);
     }
@@ -112,6 +111,9 @@ function rndStart() {
     if (document.getElementById("random").checked) {
         return startTime = rndTime();
     }
+    else {
+        return startTime = 0;
+    }
 }
 
 function playTime() {
@@ -120,27 +122,37 @@ function playTime() {
 
 function songTitle() {
     var includesongTitleFull = document.getElementById("includeSongNameFull");
+    var songTitle = document.getElementById("song-title");
 
     if (includesongTitleFull.hasAttribute("disabled")) {
         includesongTitleFull.disabled = false;
     }
     else {
         includesongTitleFull.disabled = true;
+        includesongTitleFull.checked = false;
     }
+    songTitle.innerHTML = songTitles[rnd]
 }
 
 function songTitleFull() {
-
+    document.getElementById("song-title").style.backgroundColor = "rgba(0, 0, 0, 0)";
 }
 
 
 // show anime button
 function showAnime() {
     document.getElementById("anime-title").style.backgroundColor = "rgba(0, 0, 0, 0)";
+    document.getElementById("song-title").style.backgroundColor = "rgba(0, 0, 0, 0)";
 }
 
 function hideAnime() {
-    document.getElementById("anime-title").style.backgroundColor = "black";
+    if (document.getElementById("includeSongNameFull").checked) {
+        document.getElementById("anime-title").style.backgroundColor = "black";
+    }
+    else {
+        document.getElementById("anime-title").style.backgroundColor = "black";
+        document.getElementById("song-title").style.backgroundColor = "black";
+    }
 }
 
 window.onload = init;
